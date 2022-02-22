b6d014f582b84e22f25604f3ce1a48606cf53a55
Allison Stewart <amstewart@yahoo.com>
2022-02-22 11:35:48 -0500

    added buildToken function and login endpoint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const Users = require('../users/users-model');
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const jwt = require('jsonwebtoken')
+
 
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
@@ -16,8 +20,31 @@ router.post("/register", validateRoleName, (req, res, next) => {
    */
 });
 
+function buildToken(user) {
+  const payload = {
+    subject: user.user_id, 
+    username: user.username, 
+    role_name: user.role_name
+  }
+  const options = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, JWT_SECRET, options)
+}
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
+  let { username, password } = req.body
+
+  Users.findBy({ username })
+    .then(([user]) => {
+      if(user && bcrypt.compareSync(password, user.password)) {
+        const token = buildToken(user)
+        res.status(200).json({ message: `${user.username} is back!`, token})
+      } else {
+        next({ status: 401, message: 'Invalid credentials'})
+      }
+    }) .catch(next)
+
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
 
